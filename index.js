ef7ee9a579f4ea22b30a7c7b4259f6a2bb0263b5
AbdulSamiShaikh990 <abdulsamishaikh990@gmail.com>
2024-09-26 20:48:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let speech= new SpeechSynthesisUtterance();
+
+let voices=[];
+
+let voiceSelect= document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => 
+{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]; 
+    voiceSelect.innerHTML = ''; 
+
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i); 
+        voiceSelect.add(option); 
+    });
+};
+
+voiceSelect.addEventListener("change", () => 
+{
+    let selectedIndex = voiceSelect.value; 
+    speech.voice = voices[selectedIndex]; 
+});
+
+document.querySelector("button").addEventListener("click",() =>
+{
+
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+
+})
+
